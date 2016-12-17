ee4f7b56adf5ec739332fb6562108f420f5e28e6
Antonin <frantisek.jukl@gmail.com>
2016-12-17 01:38:13 +0100

    address changed

--- connect.py
+++ connect.py
@@ -8,7 +8,7 @@ Get arg, set data and create POST request
 """
 details = urllib.parse.urlencode({'user' : 'b', 'passwd' : 'a', 'temperature' : sys.argv[1]})
 details = details.encode('UTF-8')
-url = urllib.request.Request("http://silenyprojekt.4fan.cz/zpracuj.php", details)
+url = urllib.request.Request("http://silenyprojektik.4fan.cz/zpracuj.php", details)
 url.add_header("User-Agent","Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.2.149.29 Safari/525.13")
 response = urllib.request.urlopen(url).read().decode('utf8', 'ignore')
 
